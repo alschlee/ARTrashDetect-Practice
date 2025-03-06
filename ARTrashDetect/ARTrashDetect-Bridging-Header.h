389b52998674ad5628c40ff8c27b1e109737931e
alschlee <1daysixmeal@gmail.com>
2025-03-07 03:42:10 +0900

    Initial commit

---
+++ ARTrashDetect/ARTrashDetect-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  ARTrashDetect-Bridging-Header.h
+//  ARTrashDetect
+//
+//  Created by Jingyeong Lee on 3/7/25.
+//
+
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "ShaderTypes.h"
